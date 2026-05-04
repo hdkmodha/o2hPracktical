c3a0f3cc5c388be107f68199101f836aa7d5cd75
Hardik Modha <hardik.modha27@gmail.com>
2026-05-04 16:51:38 +0530

    Initial Commit

---
+++ o2hPracktical/o2hPrackticalApp.swift
@@ -0,0 +1,17 @@
+//
+//  o2hPrackticalApp.swift
+//  o2hPracktical
+//
+//  Created by Hardik Modha on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct o2hPrackticalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
